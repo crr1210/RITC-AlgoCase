68c7e28ac0c091a368d080f67e1c44a810ad15b6
Laura Chen <laurachen@lauras-air.lan>
2022-02-15 22:20:45 -0500

    version simulation 1

--- Main.py
+++ Main.py
@@ -15,10 +15,10 @@ def main():
             with requests.Session() as s:
                 s.headers.update(config.API_KEY)
                 
-                # if arbitrage.ARBITRAGE_STATUS == 0:
-                #     arbitrage.trade_arbitrage(s)
-                # else:
-                #     arbitrage.close_arbitrage(s)
+                if arbitrage.ARBITRAGE_STATUS == 0:
+                    arbitrage.trade_arbitrage(s)
+                else:
+                    arbitrage.close_arbitrage(s)
                 
                 if tender.TENDER_STATUS == 0:
                     tender.select_tender(s)
